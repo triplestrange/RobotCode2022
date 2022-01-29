c2f2ffdaf46df66fd3f65c89d560268402f80ed4
Lori Brown <lcb20010@gmail.com>
2022-01-29 15:45:50 -0500

    Fixed issues

--- src/main/java/frc/robot/commands/Command1.java
+++ src/main/java/frc/robot/commands/Command1.java
@@ -4,23 +4,12 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+public class Command1 extends CommandBase {
+  /** Creates a new Command. */
+  public Command1() {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
