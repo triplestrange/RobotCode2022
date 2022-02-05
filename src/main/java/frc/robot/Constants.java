3fc62c324bd417bcd3f75a6da0bde054eb7be944
Austin Irwin <austindirwin@gmail.com>
2022-02-05 14:53:22 -0500

    Intake class progress
    I have almost finished adding comments and created all methods. See header for more information. Pushing now before i mess around with glass

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,11 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    /*Edits by Austin, I think that this is where we put our constant variables
+      but im not confident what usually goes here so im just puting random stuff here.
+      
+    */
+    
+
+}
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,39 +2,67 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*
+  2/5/22
+  Austin Irwin
+  Code "adapted" (copied with minor changes)
+  from 2021OffseasonCode for intake
+
+  This class controls the intake of our robot and should:
+    extend intake arms using solenoids
+    retract intake arms using solenoids
+    run wheels on the intake bars to grab and pull in
+    cargo.
+ 
+  methods contain booleans  input to indicate wether the robot
+  is running in autonomous or tele-op.
+
+  What is done:
+all necesary methods are created with good reference from 2021
+libraries imported
+
+
+  What still needs done:
+initialization of constants.
+verification that methods are compatible wtih new robot.
+debugging.
+possible more, i dont know what i dont know.
+replace smartDashboard with glass
+
+
+*/
 package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-//Imports from we
 import frc.robot.Constants; 
-
-//Imports from Rev Robotics Library
 import com.revrobotics.CANSparkMax; 
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType; 
-
-//Imports from WPILib
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-//import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//we wont be using smart dashboard. need to import glass
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 
 
 public class Intake extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  
-  /*Constants class has not yet been filled inwith constant values
+
+  //intializes local variables
+
+    /*Constants class has not yet been filled inwith constant values
     so constant.intake.motor gets angry for now*/
-  private final CANSparkMax intakeMotor = new CANSparkMax(Constants.Intake.motor,MotorType.kBrushless);
-  private final DoubleSolenoid intakeSolenoid = new DoubleSolenoid(null, 0,1);
+  private final CANSparkMax intakeMotor 
+    = new CANSparkMax(Constants.Intake.motor,MotorType.kBrushless);
+  private final DoubleSolenoid intakeSolenoid 
+    = new DoubleSolenoid(null, 0,1);
   private boolean extended = false;
   private double speedIn;
   private double speedOut;
+  private double autoSpeed; //.5
 
+  /*
+   */
   public Intake() {
     super();
     intakeMotor.restoreFactoryDefaults();
@@ -46,33 +74,64 @@ public class Intake extends SubsystemBase {
     intakeMotor.burnFlash();
   }
 
-  /*method to extend the intake
-    input: joystick is ...
-     auto is...
 
+  /*method to extend the intake
+  input: joystick is assignming a joystick for intake input
+    auto=true: robot is running in autonomouas
+    auto=false: robot is taking controller input
   */
   public void extend (Joystick joystick, boolean auto) {
     if (auto) {
       intakeSolenoid.set(Value.kForward);
       setExtended(true);
-      intakeMotor.set(.5);
+      intakeMotor.set(autoSpeed); 
+    }
+    else {
+      //we arent using smartdashboard, will have to change this
+      //double modifier = SmartDasboard.getNumber("Intake Speed",0.45);
+      intakeSolenoid.set(Value.kForward);
+      setExtended(true);
+      double speedIn = joystick.getRawAxis(3);
+      double speedOut = joystick.getRawAxis(2);
+
+      if (speedIn > 0.1)
+          intakeMotor.set(speedIn*modifier);
+      else if (speedOut > 0.1)
+          intakeMotor.set(-speedOut);
+      else
+          intakeMotor.set(0);
+      
     }
 
 
   }
 
   //method to retract the intake
-  public void retract () {}
+  public void retract () {
+    intakeSolenoid.set(Value.kReverse);
+    setExtended(false);
+  }
+
+  public void runWheels(double speed, boolean auto) {
+    if(auto){
+      intakeMotor.set(autoSpeed);
+    }
+    else{
+      intakeMotor.set(speed);}
+
+  }
 
 
+  /***********************getters and setters****************/
+  
   //Getter for the boolean variable extended
   public boolean getExtended() {
     return extended;
   }
 
   //setter for the boolean variable extended
-  public void setExtended(boolean newExtended) {
-    this.extended = newExtended;
+  public void setExtended(boolean Extended) {
+    this.extended = Extended;
   }
 
 
